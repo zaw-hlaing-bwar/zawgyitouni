6eeb754b461fa964e46bbfae7cab0a830aee0b08
Bwar <zawhlaingbwarr@gmail.com>
2019-11-13 11:54:19 +0630

    {concurrency}

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-var bodyParser     =        require("body-parser");
-const path = require('path')
-const { zgyi2uni } = require('./zgyi-uni')
-const PORT = process.env.PORT || 5000
+const throng = require('throng')
 
-express()
+const PORT = process.env.PORT || 5000
+var WORKERS = process.env.WEB_CONCURRENCY || 1;
+
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start)
+
+function start() {
+  const cool = require('cool-ascii-faces')
+  const express = require('express')
+  var bodyParser = require("body-parser");
+  const path = require('path')
+  const { zgyi2uni } = require('./zgyi-uni')
+
+
+  express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
@@ -15,3 +26,5 @@ express()
   .get('/cool', (req, res) => res.send(cool()))
   .post('/zg2uni', (req, res) => zgyi2uni(req, res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+}
